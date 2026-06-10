3a5dfa082a90fb528f289637f16ba07dfc5ba8fe
Li <121599449+baelun@users.noreply.github.com>
2026-06-10 22:49:04 +0800

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.py
@@ -0,0 +1,83 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+        def merge(l1,l2):
+            dummy = ListNode()
+            cur = dummy
+
+            while l1 and l2:
+
+                if l1.val < l2.val:
+                    cur.next = l1
+                    l1 = l1.next
+                else:
+                    cur.next = l2
+                    l2 = l2.next
+                cur = cur.next
+            
+            cur.next = l1 or l2
+
+            return dummy.next
+        if lists == []:
+            return None
+
+        while len(lists) > 1:
+            merge_list = []
+            for i in range(0,len(lists),2):
+                l1 = lists[i]
+                l2 = lists[i+1] if i+1 < len(lists) else None
+                
+                merge_list.append(merge(l1,l2))
+                
+            lists=merge_list
+        return lists[0]
+
+
+
+
+
+
+
+
+
+
+        # def merge(l1,l2):
+        #     dummy = ListNode()
+        #     cur = dummy
+        #     while l1 and l2:
+        #         if l1.val < l2.val:
+        #             cur.next = l1
+        #             l1=l1.next
+        #         else:
+        #             cur.next = l2
+        #             l2=l2.next
+                
+        #         cur = cur.next
+            
+        #     cur.next = l1 or l2 #把剩下的加進來
+            
+        #     return dummy.next
+
+
+        # if not lists:
+        #         return None
+        # while len(lists) > 1:
+        #     merge_list = []
+            
+        #     for i in range(0,len(lists),2):
+
+                
+        #         l1 = lists[i]
+        #         l2 = lists[i + 1] if i + 1 < len(lists) else None
+
+        #         merge_list.append(merge(l1,l2)) #每做完一次Merge就append到mergelist
+              
+        
+        #     lists = merge_list  # 更新merge後的結果
+        # return lists[0]
